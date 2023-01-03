a01297f2b9561ae1f4b086499bfd9a2e861c2099
Araucária Digital <110396954+Araucariadigital@users.noreply.github.com>
2023-01-03 17:29:55 -0400

    Atualização

--- assets/js/roda_vida.js
+++ assets/js/roda_vida.js
@@ -1,102 +1,6 @@
 $(document).ready(()=>{
-  var chart = AmCharts.makeChart( "chartdiv", {
-    "type": "radar",
-    "theme": "light",
-    "dataProvider": [ {
-      "country": "Habilidades e Competências"
-    }, {
-      "country": "Nivelamento"
-    }, {
-      "country": "Estratégias e Hábitos de Estudo"
-    }, {
-      "country": "Saúde Física"
-    }, {
-      "country": "Saúde Mental"
-    }, {
-      "country": "Bem-Estar"
-    }, {
-      "country": "Equidade"
-    }, {
-      "country": "Integração"
-    }, {
-      "country": "Acessibilidade e Autonomia"
-    }, {
-      "country": "Proteção Integral"
-    }, {
-      "country": "Responsabilidade Social"
-    }, {
-      "country": "Cidadania e Convivência"
-    } ],
-    "valueAxes": [ {
-      "gridType": "circles",
-      "axisTitleOffset": 20,
-      "minimum": 0,
-      "maximum": 10,
-      "axisAlpha": 0.15,
-      "guides": [
-        {},
-        {},
-        {},
-        {},
-        {},
-        {},
-        {},
-        {}
-      ]
-    } ],
-    "graphs": [ {} ],
-    "categoryField": "country"
-  });
-  
-  /**
-   * Sets value for particular index
-   */
-  
-  $("#btnPDF").hide()
-  let contagem = 0
-  
-  function setValue(index, value) {
-    
-    // Init starting positions
-    var startAngle = 270;
-    var angle = Math.round(360 / chart.dataProvider.length);
-    
-    // Create a guide
-    var guide = new AmCharts.Guide();
-    guide.angle = startAngle + angle * index;
-    guide.fillAlpha = 1;
-    guide.fillColor = chart.colors[index];
-    guide.tickLength = 0;
-    guide.toAngle = guide.angle + angle;
-    guide.toValue = value;
-    guide.value = 0;
-    guide.lineAlpha = 0;
-    
-    // Add guide
-    chart.valueAxes[0].guides[index] = guide;
-    
-    // Update category
-    chart.dataProvider[index]["country"] = chart.dataProvider[index]["country"] + " " + value;
-    
-    if(contagem == 12){
-      $("#btnPDF").show()
-      console.log('igual a 12')}
-    
-    // Update chart
-    chart.validateNow(true);
-    
-    // Reveal next question
-    var areas = document.getElementsByClassName("area");
-    for(var i = 0; i < areas.length; i++) {
-      areas[i].style.display = (index + 1) === i ? "block" : "none";
-    }
-  }
-  
-  $("#btnPDF").click(()=>{
-      window.print()
-  })
 
-  function inserirDadosNaRoda(){
+  function salvarDados(){
     $("#quiz1, #quiz2, #quiz3, #quiz4, #resultado").hide()
 
     class areasVidas {
@@ -113,7 +17,7 @@ $(document).ready(()=>{
     })
     
 
-    // ******************************************* AREA APRENDIZAGEM E DESENVOLVIMENTO *******************************************
+    // ******************************************* AREA RECUPERAR DADOS *******************************************
     $("#btn1").click(()=>{
       let ad_1 = document.querySelector("input[name='ad_1']:checked").value
       let ad_2 = document.querySelector("input[name='ad_2']:checked").value
@@ -139,15 +43,12 @@ $(document).ready(()=>{
       }else{
         mediaHE = (ad_1 + ad_2 + ad_3) / 3
         console.log(mediaHE)
-        setValue(0, Math.round(mediaHE))
 
         mediaNL = (ad_4 + ad_5 + ad_6) / 3
         console.log(mediaNL)
-        setValue(0, Math.round(mediaNL))
 
         mediaEHE = (ad_4 + ad_5 + ad_6) / 3
         console.log(mediaEHE)
-        setValue(0, Math.round(mediaEHE))
 
         // salvando no logalStorage
         let salvarHE = new areasVidas(0, Math.round(mediaHE))
@@ -193,15 +94,12 @@ $(document).ready(()=>{
       }else{
         mediaSF = (si_1 + si_2 + si_3) / 3
         console.log(mediaSF)
-        setValue(0, Math.round(mediaSF))
 
         mediaSM = (si_4 + si_5 + si_6) / 3
         console.log(mediaSM)
-        setValue(0, Math.round(mediaSM))
 
         mediaBE = (si_4 + si_5 + si_6) / 3
         console.log(mediaBE)
-        setValue(0, Math.round(mediaBE))
 
         // salvando no logalStorage
         let salvarSF = new areasVidas(3, Math.round(mediaSF))
@@ -247,15 +145,12 @@ $(document).ready(()=>{
       }else{
         mediaEQ = (dh_1 + dh_2 + dh_3) / 3
         console.log(mediaEQ)
-        setValue(0, Math.round(mediaEQ))
 
         mediaIT = (dh_4 + dh_5 + dh_6) / 3
         console.log(mediaIT)
-        setValue(0, Math.round(mediaIT))
 
         mediaAA = (dh_4 + dh_5 + dh_6) / 3
         console.log(mediaAA)
-        setValue(0, Math.round(mediaAA))
 
         // salvando no logalStorage
         let salvarEQ = new areasVidas(6, Math.round(mediaEQ))
@@ -301,15 +196,12 @@ $(document).ready(()=>{
       }else{
         mediaPI = (gd_1 + gd_2 + gd_3) / 3
         console.log(mediaPI)
-        setValue(0, Math.round(mediaPI))
 
         mediaRS = (gd_4 + gd_5 + gd_6) / 3
         console.log(mediaRS)
-        setValue(0, Math.round(mediaRS))
 
         mediaCC = (gd_4 + gd_5 + gd_6) / 3
         console.log(mediaCC)
-        setValue(0, Math.round(mediaCC))
 
         // salvando no logalStorage
         let salvarPI = new areasVidas(9, Math.round(mediaPI))
@@ -325,7 +217,8 @@ $(document).ready(()=>{
         localStorage.setItem("cidadaniaConvivencia", salvandoCC)
 
         $("#quiz4").remove()
-        $("#resultado").show() 
+        recuperaDados()
+        $("#resultado").show()
       }    
     })    
 
@@ -335,5 +228,170 @@ $(document).ready(()=>{
     })
   }
 
-  inserirDadosNaRoda()
+  salvarDados()
+
+  function recuperaDados(){
+    // Create root element
+    // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+    var root = am5.Root.new("chartdiv");
+
+    // Create chart
+    // https://www.amcharts.com/docs/v5/charts/radar-chart/
+    var chart = root.container.children.push(am5radar.RadarChart.new(root, {
+        panX: false,
+        panY: false,
+        wheelX: "12",
+        wheelY: "12"
+    }));
+
+    // Create axes and their renderers
+    // https://www.amcharts.com/docs/v5/charts/radar-chart/#Adding_axes
+    var xRenderer = am5radar.AxisRendererCircular.new(root, {});
+    xRenderer.labels.template.setAll({
+        radius: 10
+    });
+
+    var xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
+        maxDeviation: 0,
+        categoryField: "category",
+        renderer: xRenderer,
+        tooltip: am5.Tooltip.new(root, {})
+    }));
+
+    var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+        min: 0,
+        max: 10,
+        renderer: am5radar.AxisRendererRadial.new(root, {
+            minGridDistance: 20
+        })
+    }));
+
+    // Deixar os valores invisiveis
+    // yAxis.get("renderer").labels.template.set("forceHidden", true);
+
+    // Create series
+    // https://www.amcharts.com/docs/v5/charts/radar-chart/#Adding_series
+    var series = chart.series.push(am5radar.RadarColumnSeries.new(root, {
+        xAxis: xAxis,
+        yAxis: yAxis,
+        valueYField: "value",
+        categoryXField: "category"
+    }));
+
+    series.columns.template.setAll({
+        tooltipText: "{categoryX}: {valueY}",
+        templateField: "columnSettings",
+        strokeOpacity: 0,
+        width: am5.p100
+    });
+
+    // Set data
+    // https://www.amcharts.com/docs/v5/charts/radar-chart/#Setting_data
+
+      // Recupera o json do localStorage
+      let heCompetencias = JSON.parse(localStorage.getItem('habilidadesEcompetencias'))
+      let nivelamento = JSON.parse(localStorage.getItem('nivelamento'))
+      let estHabitoEstudo = JSON.parse(localStorage.getItem('estHabitoEstudo'))
+      let saudeFisica = JSON.parse(localStorage.getItem('saudeFisica'))
+      let saudeMental = JSON.parse(localStorage.getItem('saudeMental'))
+      let bemEstar = JSON.parse(localStorage.getItem('bemEstar'))
+      let equidade = JSON.parse(localStorage.getItem('equidade'))
+      let integracao = JSON.parse(localStorage.getItem('integracao'))
+      let acesAutonomia = JSON.parse(localStorage.getItem('acessibilidadeAutonomia'))
+      let protIntegral = JSON.parse(localStorage.getItem('protecaoIntegral'))
+      let respSocial = JSON.parse(localStorage.getItem('responsabilidadeSocial'))
+      let cidConvivencia = JSON.parse(localStorage.getItem('cidadaniaConvivencia'))
+
+      if(heCompetencias){var hc = (heCompetencias.nota);}
+      if(nivelamento){var nv = (nivelamento.nota);}
+      if(estHabitoEstudo){var ee = (estHabitoEstudo.nota);}
+      if(saudeFisica){var sf = (saudeFisica.nota);}
+      if(saudeMental){var sm = (saudeMental.nota);}
+      if(bemEstar){var be = (bemEstar.nota);}
+      if(equidade){var eq = (equidade.nota);}
+      if(integracao){var it = (integracao.nota);}
+      if(acesAutonomia){var aa = (acesAutonomia.nota);}
+      if(protIntegral){var pi = (protIntegral.nota);}
+      if(respSocial){var rs = (respSocial.nota);}
+      if(cidConvivencia){var cc = (cidConvivencia.nota);}
+
+
+    var data = [{
+        category: "teste1",
+        value: hc,
+        columnSettings: {
+            fill: am5.color("rgba(107, 89, 162, 0.5)")
+        }
+    }, {
+        category: "teste2",
+        value: nv,
+        columnSettings: {
+            fill: am5.color("rgb(242, 151, 178)")
+        }
+    }, {
+        category: "teste3",
+        value: ee,
+        columnSettings: {
+            fill: am5.color("rgb(139, 20, 59)")
+        }
+    }, {
+        category: "teste4",
+        value: sf,
+        columnSettings: {
+            fill: am5.color("rgb(230, 33, 71)")
+        }
+    }, {
+        category: "teste5",
+        value: sm,
+        columnSettings: {
+            fill: am5.color("rgb(107, 89, 162)")
+        }
+    }, {
+        category: "teste6",
+        value: be,
+        columnSettings: {
+            fill: am5.color("rgb(242, 151, 178)")
+        }
+    }, {
+        category: "teste7",
+        value: eq,
+        columnSettings: {
+            fill: am5.color("rgb(139, 20, 59)")
+        }
+    },{
+        category: "teste8",
+        value: it,
+        columnSettings: {
+            fill: am5.color("rgb(230, 33, 71)")
+        }
+    }, {
+        category: "teste9",
+        value: aa,
+        columnSettings: {
+            fill: am5.color("rgb(107, 89, 162)")
+        }
+    }, {
+        category: "teste10",
+        value: pi,
+        columnSettings: {
+            fill: am5.color("rgb(242, 151, 178)")
+        }
+    }, {
+        category: "teste11",
+        value: rs,
+        columnSettings: {
+            fill: am5.color("rgb(139, 20, 59)")
+        }
+    }, {
+        category: "teste12",
+        value: cc,
+        columnSettings: {
+            fill: am5.color("rgb(230, 33, 71)")
+        }
+    }];
+
+    series.data.setAll(data);
+    xAxis.data.setAll(data);
+  }
+
 })
